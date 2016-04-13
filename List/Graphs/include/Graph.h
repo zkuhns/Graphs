11796c09e971bd7ae0a5495e92c39913a866b854
zkuhns <kuhns.zachery@gmail.com>
2016-04-13 11:10:04 -0600

    Added function skeleton, added directed and weighted getters and setters.

--- List/Graphs/include/Graph.h
+++ List/Graphs/include/Graph.h
@@ -1,14 +1,38 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <vector>
+#include <string>
 
+//List
 class Graph
 {
     public:
         Graph();
         virtual ~Graph();
+
+        void set_directed(bool directed);
+        bool is_directed();
+        void set_weighted(bool weighted);
+        bool is_weighted();
+
+        bool has_vertex(std::string);
+        bool insert_vertex(std::string);
+        bool remove_vertex(std::string);
+
+        bool has_edge(std::string);
+        bool insert_edge(std::string);
+        bool remove_edge(std::string);
     protected:
     private:
+        std::vector< std::string > vertices;
+        std::vector< std::string > edges;
+
+        int vertex_count;
+        int edge_count;
+
+        bool weighted;
+        bool directed;
 };
 
 #endif // GRAPH_H
--- List/Graphs/src/Graph.cpp
+++ List/Graphs/src/Graph.cpp
@@ -1,11 +1,33 @@
 #include "Graph.h"
 
+//List
+
 Graph::Graph()
 {
-    //ctor
+    this->weighted = 0;
+    this->directed = 0;
+
+    this->vertex_count = 0;
+    this->edge_count = 0;
 }
 
 Graph::~Graph()
 {
     //dtor
+}
+
+void Graph::set_directed(bool directed) {
+    this->directed = directed;
+}
+
+bool Graph::is_directed() {
+    return directed;
+}
+
+void Graph::set_weighted(bool weighted) {
+    this->weighted = weighted;
+}
+
+bool Graph::is_weighted() {
+    return weighted;
 }
